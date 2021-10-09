35c0873a7e44005f165ad2252005c4147b63fbbf
Administrator <Administrator@HRIDOYPCWORK>
2021-10-09 13:06:27 +0600

    Authontication Added

---
+++ ApiConst.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace MinimalApi
+{
+    public class ApiConst
+    {
+        public const string Issuer = "Hridoy";
+        public const string Audience = "MinimalAppUser";
+        public const string key = "28216481291932476";
+
+        public const string AuthSchemes = "Identity.Application" + "," + JwtBearerDefaults.AuthenticationScheme;
+        public static object CreateToken(JwtToken model) 
+        {
+            var user = "hridoy";
+            var password = "12345678";
+            if(user==model.EmployeeId && password == model.Password)
+            {
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ApiConst.key));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                           ApiConst.Issuer,
+                           ApiConst.Audience,
+                           expires: DateTime.UtcNow.AddHours(2),
+                           signingCredentials: creds
+                        );
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+            else
+            {
+                return "Failed";
+            }
+            
+        }
+    }
+    public class JwtToken
+    {
+        public string EmployeeId { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,78 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using MinimalApi;
 using MinimalApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<CertificateMSContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("CertificateDb")));
+
+//Configure Services starts 
+    builder.Services.AddDbContext<CertificateMSContext>(o =>
+        o.UseSqlServer(builder.Configuration.GetConnectionString("CertificateDb")));
+    builder.Services.AddEndpointsApiExplorer();
+#region Swagger authorization config     
+//builder.Services.AddSwaggerGen(x => 
+//{
+//    x.AddSecurityDefinition("JwtBearer", new OpenApiSecurityScheme 
+//    {
+//        Description ="Jwt Authoentication header using Bearer Scheme",
+//        Name ="Authorization",
+//        In=ParameterLocation.Header,
+//        Type=SecuritySchemeType.ApiKey
+//    });
+//    x.AddSecurityRequirement(new OpenApiSecurityRequirement 
+//    {
+//        {new OpenApiSecurityScheme{Reference=new OpenApiReference
+//        {
+//            Id="JwtBearer",
+//            Type=ReferenceType.SecurityScheme
+//        }}, new List<string>()}
+//    });
+//});
+#endregion 
+builder.Services.AddSwaggerGen();
+    //builder.Services
+    //    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    //    .AddJwtBearer();
+
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = "JwtBearer";
+        options.DefaultChallengeScheme = "JwtBearer";
+    }).AddJwtBearer("JwtBearer", jwtoptions =>
+    {
+        jwtoptions.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = ApiConst.Issuer,
+            ValidAudience = ApiConst.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ApiConst.key)),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true
+        };
+    });
+    builder.Services.AddAuthorization();
+//Configure services ends
+
 var app = builder.Build();
 
-app.MapGet("/api/Dept", async (CertificateMSContext _Db ) =>   await _Db.Departments.ToListAsync());
+//Configure start 
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseAuthentication();
+    app.UseAuthorization();
+//Configure ends
 
+//apies
+    app.MapPost("/api/Jwt", (JwtToken model) => ApiConst.CreateToken(model)).AllowAnonymous();
+
+       //Authoriztion using attributes
+    app.MapGet("/api/Dept",[Authorize] async (CertificateMSContext _Db ) =>   await _Db.Departments.ToListAsync());
+
+      //Using fluid Authorization assignment
+    app.MapGet("/api/Deptt", async (CertificateMSContext _Db) => await _Db.Departments.ToListAsync()).RequireAuthorization();
+//apies end 
 app.Run();
